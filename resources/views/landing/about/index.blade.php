97d378e5950060032928343558e5fc47fe9b10a2
NdaruVictor <ndaruvictor55@gmail.com>
2023-12-12 18:50:43 +0700

    add : Adjust about page

--- resources/views/landing/about/index.blade.php
+++ resources/views/landing/about/index.blade.php
@@ -1,44 +1,28 @@
 <x-landing-layout>
-    <div class="text-center mt-10">
+    <div class="text-center mt-40">
         <h2 class="text-2xl font-bold text-black">About Us</h2>
     </div>
-    <div class="w-full pt-14">
+    <div class="w-full p-14">
         <div class="h-3/4 w-3/4 mx-auto  rounded-lg bg-white shadow-lg ">
             <div class="flex flex-wrap items-center">
                 <div class="w-full shrink-0 grow-0 basis-auto flex items-center justify-center p-8">
-                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, odit iste
-                        perspiciatis alias deleniti expedita quae, non, molestias culpa ratione itaque sed? Eveniet aut
-                        in tempore et natus dolorem recusandae deleniti sit, autem laudantium totam dolores iure
-                        corrupti. Rerum sapiente assumenda ad, ipsam explicabo fugiat? Quam, adipisci quisquam saepe
-                        itaque ipsam possimus tenetur vero quia labore dignissimos dolor expedita aut perspiciatis
-                        dolorem beatae quasi enim exercitationem odio id. Ullam, sit eius totam omnis, alias ut tempore
-                        magnam deserunt, sapiente eum ad molestias natus perferendis sint itaque porro? Sed animi,
-                        voluptate maiores rerum praesentium reiciendis asperiores impedit illum id numquam? Dolor a
-                        perferendis doloremque consequuntur aliquam, sequi accusantium ipsam maiores qui quo, fuga
-                        facilis. Voluptate cum aut recusandae amet asperiores provident eius, excepturi molestiae ad
-                        cupiditate possimus! Nostrum expedita, totam repellat nesciunt numquam veniam excepturi corrupti
-                        laudantium tempore quo aliquam eveniet sed? Ipsum magnam doloremque aut, vitae fuga temporibus
-                        ullam mollitia cumque exercitationem eaque autem facere inventore reprehenderit, omnis fugit
-                        excepturi culpa ab aliquam nesciunt nulla tempora, fugiat vero praesentium! Ab illum accusantium
-                        obcaecati quod quidem eveniet. Iste saepe vel minima autem! Odit, blanditiis placeat dolorem
-                        optio dolor quam tempore obcaecati fuga labore, quidem cumque. Voluptates maiores a placeat ea,
-                        natus nihil cupiditate aperiam repudiandae autem perferendis cum aliquid illo cumque, quidem
-                        esse ducimus consectetur facere incidunt enim tenetur hic sapiente provident doloremque rem!
-                        Consequuntur consequatur neque nobis culpa praesentium dignissimos fugiat soluta, quia
-                        perspiciatis odit porro velit amet? Rerum, consequatur! Optio, suscipit nesciunt illo impedit
-                        quam facilis dolorem, blanditiis eaque mollitia, magni dicta! Tenetur voluptate consequatur
-                        cupiditate voluptas veniam debitis sequi similique pariatur? Fugit, neque velit corrupti
-                        voluptas hic veritatis commodi dolorem earum nobis repellendus! Quaerat mollitia odio odit
-                        dolores voluptatibus inventore, debitis facere quisquam unde officia expedita ullam omnis.
-                        Cumque, eveniet saepe, odit ut harum deleniti esse ipsum voluptates incidunt voluptas quaerat
-                        tempore beatae amet eaque nam! Nulla quia ut unde quo eius doloremque nesciunt neque? Excepturi
-                        quia doloribus rerum eveniet obcaecati officiis recusandae illo odit tempora, reprehenderit quos
-                        facere sit unde dignissimos minima voluptates exercitationem quae magni non voluptatum modi!
-                        Optio sapiente porro enim quos reprehenderit corrupti quas dignissimos soluta dolor sint alias,
-                        debitis ut voluptas esse quis perferendis accusamus, possimus numquam impedit ipsa quia. Quo eos
-                        aliquid est rerum minus nihil alias voluptates quasi blanditiis non, itaque ratione unde sequi
-                        impedit architecto expedita facere excepturi quae harum? Repellendus recusandae, ab sit amet
-                        iste veniam officia praesentium libero voluptatem nulla ut earum cumque vel eaque magnam placeat
+                    <p class="text-center">Welcome to VisFood - a complete culinary guide for connoisseurs
+                        food! We are a community of food bloggers dedicated to taking you on the journey
+                        unforgettable culinary delights. From appetizing menu recommendations to practical recipes
+                        that you can try in your own kitchen, we are here to inspire and make the experience easier
+                        your culinary delights. Every menu recommendation not only comes from the best places to eat, but also
+                        involving our honest and detailed personal views on each dish.
+                        We also share creative and innovative recipes, helping you explore your culinary skills
+                        Alone. Whether you are a beginner who wants to try cooking or a home chef
+                        experienced, VisFood provides the resources you need to create dishes
+                        delicious on your dining table.
+                        Plus, don't miss our reviews of great places to eat. Starting from the stall
+                        authentic street food to exclusive restaurants, we search and evaluate experiences
+                        culinary delights to ensure that every visit is memorable.
+                        Join us on a mouth-watering, globe-trotting culinary adventure
+                        food with VisFood. We are committed to being your loyal friend
+                        explore culinary delights and beauty. Enjoy reading, cooking and exploring the mind
+                        incomparable taste!
                     </p>
                 </div>
             </div>
